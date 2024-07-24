aae2c655b071ba4a9c10eda30776369e70747234
Nhật Vỹ <nguyen.pham.nhat.vy@sun-asterisk.com>
2024-07-24 09:16:48 +0700

    Init Project

---
+++ src/config/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false,
+  logging: false,
+  entities: [join(__dirname, '../entity/*.entity.{ts,js}')],
+  migrations: [join(__dirname, '../config/migrations/*.{ts,js}')],
+});
